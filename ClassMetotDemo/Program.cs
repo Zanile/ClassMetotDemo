8d8ef6a2c28a4b48bbf9f6bda8931333d4483d0a
OLGUN ONAL <OLGUN ONAL@DESKTOP-IMV1LUA>
2023-02-28 10:29:38 +0100

    Proje dosyası ekle.

---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMetotDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer = new Customer();
+            customer.Id = 123;
+            customer.Name = "Test1";
+            customer.Surname = "Test12";
+
+            Customer customer1 = new Customer();
+            customer1.Id = 321;
+            customer1.Name = "Test2";
+            customer1.Surname = "Test23";
+
+            Customer[] customers = new Customer[] { customer, customer1 };
+
+            foreach (var item in customers)
+            {
+                Console.WriteLine("Customer ID :" + item.Id);
+                Console.WriteLine("Customer Name :" + item.Name);
+                Console.WriteLine("Customer Surname :" + item.Surname);
+
+                Console.WriteLine("----------------------");
+            }
+
+            AddCustomer addCustomer = new AddCustomer();
+
+            addCustomer.CustomerAdd(customer);
+            addCustomer.CustomerAdd(customer1);
+
+            Console.WriteLine("List Metodu");
+            addCustomer.ListAdd(customer);
+            addCustomer.ListAdd(customer1);
+
+            Console.WriteLine("Delete Metedu");
+            addCustomer.CustomerDelete(customer);
+            addCustomer.CustomerDelete(customer1);
+
+        }
+    }
+
+}
---
+++ ClassMetotDemo/addCustomer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMetotDemo
+{
+    internal class AddCustomer
+    {
+        public void CustomerAdd(Customer customers)
+        {
+            Console.WriteLine("Customer ID :" + customers.Id);
+            Console.WriteLine("Customer Name :" + customers.Name);
+            Console.WriteLine("Customer Surname :" + customers.Surname);
+
+            Console.WriteLine("----------------------");
+        }
+
+        public void ListAdd(Customer customers)
+        {
+            Console.WriteLine("Yeni Musteri Eklendi" + customers.Name);
+        }
+
+        public void CustomerDelete(Customer customers)
+        {
+            Console.WriteLine("Musteri Silindi" + customers.Name);
+        }
+    }
+}
